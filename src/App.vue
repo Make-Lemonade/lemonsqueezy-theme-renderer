ce53607b809a1e777d4dde14118f5754f422d8ea
Gilbert Pellegrom <gilbert@pellegrom.me>
2022-01-19 17:07:31 +0000

    Set up App.vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <LsThemeRenderer />
+    </div>
+</template>
+
+<script>
+import LsThemeRenderer from './components/LsThemeRenderer.vue';
+
+export default {
+    components: { LsThemeRenderer },
+};
+</script>
